2fa40b23012f8c288bb3b5e7064c5d90ba0108dc
PtatG <55119325+PtatG@users.noreply.github.com>
2019-11-30 19:44:38 -0500

    Add files via upload

---
+++ app/controllers/routine.controller.js
@@ -0,0 +1,441 @@
+// written by: Phillip Tat from GROUP 21(Trevor Bland, Zach Arehart, Rob Lee, and Phillip Tat)
+// date written: 11/23/19
+// course: COP 4331(Rick Leinecker)
+// purpose: Large Project: Focus Pocus(api)
+const Routine = require("../models/routine.model.js");
+const User = require("../models/user.model.js");
+const jwt = require("../jwt/jwtFunction.js");
+const fs = require("fs");
+const bcrypt = require("bcryptjs");
+const Crypto = require("simple-crypto-js").default;
+let secretKey  = fs.readFileSync('./config/secret.key', 'utf8');
+let crypto = new Crypto(secretKey);
+
+// Create a new routine
+// JSON inputs required: token, routineName, and coffeeNap
+// outputs JSON: token, routine, and message
+// routine will always contain _id, routineName, and coffeeNap
+// routine will contain these optional fields if you pass them in:
+// pomTimer, breakTimer, pomCount, breakCount, largeBreakCount, and goalHit
+exports.create = (req, res) => {
+  // check required fields
+  if (!req.body.token) {
+    return res.status(400).json({
+      message: "Token can not be empty."
+    });
+  }
+  if (!req.body.routineName) {
+    return res.status(400).json({
+      message: "Routine name can not be empty."
+    });
+  }
+  if (req.body.coffeeNap === undefined) {
+    return res.status(400).json({
+      message: "Coffee Nap can not be empty."
+    });
+  }
+
+  let token, userId;
+  if ( jwt.verify(req.body.token) ) {
+    // decode jwt, then decrypt user ID
+    token = jwt.decode(req.body.token);
+    userId = crypto.decrypt(token.payload.userId);
+    userId = userId.substr(1, userId.length - 2);
+  }
+  else {
+    return res.status(401).json({
+      message: "Token could not be verified."
+    });
+  }
+
+  // create routine
+  const routineData = new Routine({
+    userId: userId,
+    routineName: req.body.routineName,
+    coffeeNap: req.body.coffeeNap,
+    napTimer: req.body.napTimer,
+    pomTimer: req.body.pomTimer,
+    breakTimer: req.body.breakTimer,
+    pomCount: req.body.pomCount,
+    breakCount: req.body.breakCount,
+    largeBreakCount: req.body.largeBreakCount,
+    goalHit: req.body.goalHit,
+  });
+
+  // Save the routine in the database
+  routineData.save()
+    .then(routine => {
+
+      // encrypt user ID before putting into payload
+      let cipher = crypto.encrypt(userId);
+      let payload = {
+        userId: cipher,
+      };
+      let token = jwt.sign(payload);
+
+      // remove userId field, otherwise what was the point of
+      // encrypting userId before putting into jwt
+      routine.userId = undefined;
+
+      res.json({
+        token: token,
+        routine,
+        message: "Routine " + routine.routineName + " created successfully!"
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: err.message || "Some error occurred while creating the routine."
+      });
+    });
+}; // end create
+
+// Retrieve and return all the user's routines from the database
+// JSON inputs required: token
+// outputs JSON: token, array of routines associated to the
+// userId, and message
+exports.userRoutines = (req, res) => {
+  // check required fields
+  if (!req.body.token) {
+    return res.status(400).json({
+      message: "Token can not be empty."
+    });
+  }
+
+  let token, userId;
+  if ( jwt.verify(req.body.token) ) {
+    // decode jwt, then decrypt user ID
+    token = jwt.decode(req.body.token);
+    userId = crypto.decrypt(token.payload.userId);
+    userId = userId.substr(1, userId.length - 2);
+  }
+  else {
+    return res.status(401).json({
+      message: "Token could not be verified."
+    });
+  }
+
+  // find routines based on userId
+  Routine.find({ userId: userId })
+    .then(routines => {
+      if (!routines[0]) {
+        return res.json({
+          message: "No routines for this user exist in the database."
+        });
+      }
+
+      // encrypt user ID before putting into payload
+      let cipher = crypto.encrypt(userId);
+      let payload = {
+        userId: cipher,
+      };
+      let token = jwt.sign(payload);
+
+      for (let i = 0; i < routines.length; i++) {
+        routines[i].userId = undefined;
+      }
+
+      res.json({
+        token: token,
+        routines,
+        message: "Successfully got user routines."
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: err.message || "Some error occurred while retrieving the user's routines."
+      });
+    });
+}; // end userRoutines
+
+// Edit a routine
+// JSON inputs required: token and _id
+// outputs JSON: token, updated routine with all
+// fields except userId, and message
+// don't need to check coffeeNap b/c no need to edit coffee naps
+exports.edit = (req, res) => {
+  // Check required fields
+  if (!req.body.token) {
+    return res.status(400).json({
+      message: "Token can not be empty."
+    });
+  }
+  if (!req.body._id) {
+    return res.status(400).json({
+      message: "_id can not be empty."
+    });
+  }
+
+  let token, userId;
+  if ( jwt.verify(req.body.token) ) {
+    // decode jwt, then decrypt user ID
+    token = jwt.decode(req.body.token);
+    userId = crypto.decrypt(token.payload.userId);
+    userId = userId.substr(1, userId.length - 2);
+  }
+  else {
+    return res.status(401).json({
+      message: "Token could not be verified."
+    });
+  }
+
+  // find routine by id and replace empty variables
+  Routine.findById(req.body._id)
+    .then(routine => {
+      if (!routine) {
+        return res.status(404).json({
+          message: "Routine not found with id " + req.body._id
+        });
+      }
+      if (!req.body.routineName) {
+        req.body.routineName = routine.routineName;
+      }
+      if (!req.body.pomTimer) {
+        req.body.pomTimer = routine.pomTimer;
+      }
+      if (!req.body.breakTimer) {
+        req.body.breakTimer = routine.breakTimer;
+      }
+      if (!req.body.pomCount) {
+        req.body.pomCount = routine.pomCount;
+      }
+      if (!req.body.breakCount) {
+        req.body.breakCount = routine.breakCount;
+      }
+      if (!req.body.largeBreakCount) {
+        req.body.largeBreakCount = routine.largeBreakCount;
+      }
+      if (req.body.goalHit === undefined) {
+        req.body.goalHit = routine.goalHit;
+      }
+
+      // Find user by _id and update it
+      Routine.findByIdAndUpdate(
+        req.body._id,
+        {
+          routineName: req.body.routineName,
+          pomTimer: req.body.pomTimer,
+          breakTimer: req.body.breakTimer,
+          pomCount: req.body.pomCount,
+          breakCount: req.body.breakCount,
+          largeBreakCount: req.body.largeBreakCount,
+          goalHit: req.body.goalHit
+        },
+        { new: true }
+      )
+        .then(routine => {
+          if (!routine) {
+            return res.status(404).json({
+              message: "Routine not found with id " + req.body._id
+            });
+          }
+
+          // encrypt user ID before putting into payload
+          let cipher = crypto.encrypt(userId);
+          let payload = {
+            userId: cipher,
+          };
+          let token = jwt.sign(payload);
+
+          routine.userId = undefined;
+
+          res.json({
+            token: token,
+            routine,
+            message: "Routine " + routine.routineName + " successfully updated!"
+          });
+        })
+        .catch(err => {
+          if (err.kind === "ObjectId") {
+            return res.status(404).json({
+              message: "Routine not found with id " + req.body._id
+            });
+          }
+          return res.status(500).json({
+            message: "Error updating routine with id " + req.body._id
+          });
+        });
+    })
+    .catch(err => {
+      if (err.kind === "ObjectId") {
+        return res.status(404).json({
+          message: "Routine not found with id " + req.body._id
+        });
+      }
+      return res.status(500).json({
+        message: "Error updating routine with id " + req.body._id
+      });
+    });
+}; // end edit
+
+// Delete a routine
+// JSON inputs required: token and _id
+// outputs JSON: token, routineName, and message
+exports.delete = (req, res) => {
+  // Check required fields
+  if (!req.body.token) {
+    return res.status(400).json({
+      message: "Token can not be empty."
+    });
+  }
+  if (!req.body._id) {
+    return res.status(400).json({
+      message: "_id can not be empty."
+    });
+  }
+
+  let token, userId;
+  if ( jwt.verify(req.body.token) ) {
+    // decode jwt, then decrypt user ID
+    token = jwt.decode(req.body.token);
+    userId = crypto.decrypt(token.payload.userId);
+    userId = userId.substr(1, userId.length - 2);
+  }
+  else {
+    return res.status(401).json({
+      message: "Token could not be verified."
+    });
+  }
+
+  Routine.findByIdAndDelete(req.body._id)
+    .then(routine => {
+      if (!routine) {
+        return res.status(404).json({
+          message: "Routine ID incorrect or does not exist."
+        });
+      }
+
+      // encrypt user ID before putting into payload
+      let cipher = crypto.encrypt(userId);
+      let payload = {
+        userId: cipher,
+      };
+      let token = jwt.sign(payload);
+
+      res.json({
+        token: token,
+        routineName: routine.routineName,
+        message: "Routine " + routine.routineName + " deleted successfully!"
+      });
+    })
+    .catch(err => {
+      if (err.kind === "ObjectId" || err.name === "NotFound") {
+        return res.status(404).json({
+          message: "Routine " + req.body.routineName + " not found."
+        });
+      }
+      return res.status(500).json({
+        message: "Could not find routine " + req.body.routineName + "."
+      });
+    });
+}; // end delete
+
+// Delete all routines for a specific user
+// JSON inputs required: token
+// outputs JSON: deletedCount and message
+exports.deleteAll = (req, res) => {
+  // Check required fields
+  if (!req.body.token) {
+    return res.status(400).json({
+      message: "Token can not be empty."
+    });
+  }
+
+  let token, userId;
+  if ( jwt.verify(req.body.token) ) {
+    // decode jwt, then decrypt user ID
+    token = jwt.decode(req.body.token);
+    userId = crypto.decrypt(token.payload.userId);
+    userId = userId.substr(1, userId.length - 2);
+  }
+  else {
+    return res.status(401).json({
+      message: "Token could not be verified."
+    });
+  }
+
+  Routine.deleteMany({ userId: userId })
+    .then(routine => {
+      if (!routine) {
+        return res.status(404).json({
+          message: "Routine ID incorrect or does not exist."
+        });
+      }
+
+      res.json({
+        deletedCount: routine.deletedCount,
+        message: "All routines deleted successfully!"
+      });
+    })
+    .catch(err => {
+      if (err.kind === "ObjectId" || err.name === "NotFound") {
+        return res.status(404).json({
+          message: "Routines not found."
+        });
+      }
+      return res.status(500).json({
+        message: "Could not find routines."
+      });
+    });
+}; // end deleteAll
+
+// Retrieve and return all routines from the database
+// JSON inputs required: token from admin account
+// outputs JSON: routines array with all fields
+exports.getAll = (req, res) => {
+  // Check required fields
+  if (!req.body.token) {
+    return res.status(400).json({
+      message: "Token can not be empty."
+    });
+  }
+
+  let token, userId;
+  if ( jwt.verify(req.body.token) ) {
+    // decode jwt, then decrypt user ID
+    token = jwt.decode(req.body.token);
+    userId = crypto.decrypt(token.payload.userId);
+    // for some reason, decrypt has double quotes which need
+    // to be removed from both front and back of the string
+    userId = userId.substr(1, userId.length - 2);
+  }
+  else {
+    return res.status(401).json({
+      message: "Token could not be verified."
+    });
+  }
+
+  User.findOne({ _id: userId, admin: true })
+    .then(user => {
+      if (!user) {
+        return res.json({
+          message: "User does not exist or does not have admin access."
+        });
+      }
+
+      Routine.find()
+        .then(routines => {
+          if (!routines[0]) {
+            return res.json({
+              message: "No routines exist in the database."
+            });
+          }
+          res.json({ routines });
+        })
+        .catch(err => {
+          res.status(500).json({
+            message: err.message || "Some error occurred while retrieving routines."
+          });
+        });
+    })
+    .catch(err => {
+      if (err.kind === "ObjectId") {
+        return res.status(404).json({
+          message: "User not found with provided ID"
+        });
+      }
+      res.status(500).json({
+        message: err.message || "Some error occurred while retrieving Users."
+      });
+    });
+}; // end getAll
--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,126 +1,375 @@
+// written by: Phillip Tat from GROUP 21(Trevor Bland, Zach Arehart, Rob Lee, and Phillip Tat)
+// date written: 11/23/19
+// course: COP 4331(Rick Leinecker)
+// purpose: Large Project: Focus Pocus(api)
 const User = require("../models/user.model.js");
+const jwt = require("../jwt/jwtFunction.js");
+const fs = require("fs");
+const bcrypt = require("bcryptjs");
+const Crypto = require("simple-crypto-js").default;
+let secretKey  = fs.readFileSync('./config/secret.key', 'utf8');
+let crypto = new Crypto(secretKey);
 
 // Create and Save a new user
-exports.create = (req, res) => {
-  // Validate request
+// JSON inputs required: username, email, and password
+// outputs JSON: username and message
+exports.register = (req, res) => {
+  // Check required fields
+  if (!req.body.username) {
+    return res.status(400).json({
+      message: "Username can not be empty."
+    });
+  }
+  if (!req.body.email) {
+    return res.status(400).json({
+      message: "Email can not be empty."
+    });
+  }
   if (!req.body.password) {
-    return res.status(400).send({
-      message: "Password can not be empty"
+    return res.status(400).json({
+      message: "Password can not be empty."
     });
   }
 
   // Create a user
   const user = new User({
-    user: req.body.user || "Untitled User",
-    password: req.body.password
+    username: req.body.username,
+    email: req.body.email,
+    fullName: req.body.fullName,
+    password: req.body.password,
+    admin: false
   });
 
+  user.password = bcrypt.hashSync(user.password, 10);
+
   // Save user in the database
-  user
-    .save()
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: err.message || "Some error occurred while creating the User."
-      });
+  user.save()
+  .then(data => {
+    res.json({
+      username: data.username,
+      message: "User " + data.username + " registered successfully!"
     });
-};
-
-// Retrieve and return all users from the database.
-exports.findAll = (req, res) => {
-  User.find()
-    .then(users => {
-      res.send(users);
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: err.message || "Some error occurred while retrieving Users."
-      });
+  })
+  .catch(err => {
+    res.status(500).json({
+      message: err.message || "An error occurred while registering the user."
     });
-};
+  });
+}; // end register
 
-// Find a single user with a userId
-exports.findOne = (req, res) => {
-  User.findById(req.params.userId)
-    .then(user => {
-      if (!user) {
-        return res.status(404).send({
-          message: "User not found with id " + req.params.userId
-        });
-      }
-      res.send(user);
-    })
-    .catch(err => {
-      if (err.kind === "ObjectId") {
-        return res.status(404).send({
-          message: "User not found with id " + req.params.userId
-        });
-      }
-      return res.status(500).send({
-        message: "Error retrieving User with id " + req.params.userId
-      });
+// Login a user
+// JSON inputs required: username and password
+// outputs JSON: token, username, email, fullName,
+// createdAt, updatedAt, and message
+exports.login = (req, res) => {
+  // Check required fields
+  if (!req.body.username) {
+    return res.status(400).json({
+      message: "Username can not be empty."
     });
-};
-
-// Update a user identified by the userId in the request
-exports.update = (req, res) => {
-  // Validate Request
+  }
   if (!req.body.password) {
-    return res.status(400).send({
-      message: "User password can not be empty"
+    return res.status(400).json({
+      message: "Password can not be empty."
     });
   }
 
-  // Find user and update it with the request body
-  User.findByIdAndUpdate(
-    req.params.userId,
-    {
-      user: req.body.user || "Untitled User",
-      password: req.body.password
-    },
-    { new: true }
-  )
+  // find one user based on username
+  User.findOne({ username: req.body.username })
     .then(user => {
       if (!user) {
-        return res.status(404).send({
-          message: "User not found with id " + req.params.userId
+        return res.status(500).json({
+          message: "Could not find user."
         });
       }
-      res.send(user);
+
+      // check that the passwords match
+      if ( !bcrypt.compareSync(req.body.password, user.password) ) {
+        return res.json({
+          message: "Incorect password."
+        });
+      }
+
+      // encrypt user ID before putting into payload
+      let cipher = crypto.encrypt(user._id);
+      let payload = {
+        userId: cipher,
+      };
+      let token = jwt.sign(payload);
+
+      res.json({
+        token: token,
+        username: user.username,
+        email: user.email,
+        fullName: user.fullName,
+        createdAt: user.createdAt,
+        updatedAt: user.updatedAt,
+        message: "User " + user.username + " login successfull!"
+      });
     })
     .catch(err => {
       if (err.kind === "ObjectId") {
-        return res.status(404).send({
-          message: "User not found with id " + req.params.userId
+        return res.status(404).json({
+          message: "Username is of the wrong kind."
         });
       }
-      return res.status(500).send({
-        message: "Error updating user with id " + req.params.userId
+      return res.status(404).json({
+        message: "Could not find username " + req.body.username + "."
       });
     });
-};
+}; // end login
 
-// Delete a user with the specified userId in the request
-exports.delete = (req, res) => {
-  User.findByIdAndRemove(req.params.userId)
+// Edit user
+// JSON inputs required: token
+// outputs JSON: token, username, email, fuullName, and message
+exports.edit = (req, res) => {
+  // Check required fields
+  if (!req.body.token) {
+    return res.status(400).json({
+      message: "Token can not be empty."
+    });
+  }
+
+  let token, userId;
+  if ( jwt.verify(req.body.token) ) {
+    // decode jwt, then decrypt user ID
+    token = jwt.decode(req.body.token);
+    userId = crypto.decrypt(token.payload.userId);
+    // for some reason, decrypt has double quotes which need
+    // to be removed from both front and back of the string
+    userId = userId.substr(1, userId.length - 2);
+  }
+  else {
+    return res.status(401).json({
+      message: "Token could not be verified."
+    });
+  }
+
+  // Find user by id and replace empty variables
+  User.findById(userId)
     .then(user => {
       if (!user) {
-        return res.status(404).send({
-          message: "User not found with id " + req.params.userId
+        return res.status(404).json({
+          message: "User not found with provided ID"
         });
       }
-      res.send({ message: "User deleted successfully!" });
+      if (!req.body.email) {
+        req.body.email = user.email;
+      }
+      if (!req.body.fullName) {
+        req.body.fullName = user.fullName;
+      }
+      // need to if else here because if password exists,
+      // that means it needs to be replaced, so the password
+      // must first be hashed before storing in the database
+      if (!req.body.password) {
+        req.body.password = user.password;
+      }
+      else {
+        req.body.password = bcrypt.hashSync(req.body.password, 10);
+      }
+
+      // Find user by _id and update it
+      User.findByIdAndUpdate(
+        user._id,
+        {
+          email: req.body.email,
+          fullName: req.body.fullName,
+          password: req.body.password
+        },
+        { new: true }
+      )
+        .then(user => {
+          if (!user) {
+            return res.status(404).json({
+              message: "User not found with provided ID"
+            });
+          }
+
+          // encrypt user ID before putting into payload
+          let cipher = crypto.encrypt(user._id);
+          let payload = {
+            userId: cipher,
+          };
+          let token = jwt.sign(payload);
+
+          res.json({
+            token: token,
+            username: user.username,
+            email: user.email,
+            fullName: user.fullName,
+            message: "User " + user.username + " successfully updated!"
+          });
+        })
+        .catch(err => {
+          if (err.kind === "ObjectId") {
+            return res.status(404).json({
+              message: "User not found with id provided ID"
+            });
+          }
+          return res.status(500).json({
+            message: "Error updating user with id provided ID"
+          });
+        });
     })
     .catch(err => {
-      if (err.kind === "ObjectId" || err.name === "NotFound") {
-        return res.status(404).send({
-          message: "User not found with id " + req.params.userId
+      if (err.kind === "ObjectId") {
+        return res.status(404).json({
+          message: "User not found with provided ID"
         });
       }
-      return res.status(500).send({
-        message: "Could not delete user with id " + req.params.userId
+      return res.status(500).json({
+        message: "Error updating user with provided ID"
       });
     });
-};
+}; // end edit
+
+// Delete a user
+// JSON inputs required: token, username, and password
+// outputs JSON: username and message
+exports.delete = (req, res) => {
+  // Check required fields
+  if (!req.body.token) {
+    return res.status(400).json({
+      message: "Token can not be empty."
+    });
+  }
+  if (!req.body.username) {
+    return res.status(400).json({
+      message: "Username can not be empty."
+    });
+  }
+  if (!req.body.password) {
+    return res.status(400).json({
+      message: "Password can not be empty."
+    });
+  }
+
+  let token, userId;
+  if ( jwt.verify(req.body.token) ) {
+    // decode jwt, then decrypt user ID
+    token = jwt.decode(req.body.token);
+    userId = crypto.decrypt(token.payload.userId);
+    userId = userId.substr(1, userId.length - 2);
+  }
+  else {
+    return res.status(401).json({
+      message: "Token could not be verified."
+    });
+  }
+
+  // check that userId matches
+  User.findById(userId)
+  .then(user => {
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found with provided ID"
+      });
+    }
+
+    // check that the passwords match
+    if ( !bcrypt.compareSync(req.body.password, user.password) ) {
+      return res.json({
+        message: "Incorect password."
+      });
+    }
+
+    // check username and password matches before deleting
+    User.findOneAndDelete({
+      _id: userId,
+      username: req.body.username
+    })
+      .then(user => {
+        if (!user) {
+          return res.status(404).json({
+            message: "Username is incorrect."
+          });
+        }
+        res.json({
+          username: user.username,
+          message: "User " + user.username + " deleted successfully!"
+        });
+      })
+      .catch(err => {
+        if (err.kind === "ObjectId" || err.name === "NotFound") {
+          return res.status(404).json({
+            message: "User " + req.body.username + " not found."
+          });
+        }
+        return res.status(500).json({
+          message: "Could not find user " + req.body.username + "."
+        });
+      });
+  })
+  .catch(err => {
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({
+        message: "User not found with provided ID"
+      });
+    }
+    return res.status(500).json({
+      message: "Error finding user with provided ID"
+    });
+  });
+}; // end delete
+
+// Retrieve and return all users from the database
+// JSON inputs required: token from admin account
+// outputs JSON: users array with all fields
+exports.getAll = (req, res) => {
+  // Check required fields
+  if (!req.body.token) {
+    return res.status(400).json({
+      message: "Token can not be empty."
+    });
+  }
+
+  let token, userId;
+  if ( jwt.verify(req.body.token) ) {
+    // decode jwt, then decrypt user ID
+    token = jwt.decode(req.body.token);
+    userId = crypto.decrypt(token.payload.userId);
+    // for some reason, decrypt has double quotes which need
+    // to be removed from both front and back of the string
+    userId = userId.substr(1, userId.length - 2);
+  }
+  else {
+    return res.status(401).json({
+      message: "Token could not be verified."
+    });
+  }
+
+  User.findOne({ _id: userId, admin: true })
+    .then(user => {
+      if (!user) {
+        return res.json({
+          message: "This user does not exist or does not have admin access."
+        });
+      }
+
+      User.find()
+        .then(users => {
+          if (!users[0]) {
+            return res.json({
+              message: "No users exist in the database."
+            });
+          }
+          res.json({ users });
+        })
+        .catch(err => {
+          res.status(500).json({
+            message: err.message || "Some error occurred while retrieving Users."
+          });
+        });
+   })
+    .catch(err => {
+      if (err.kind === "ObjectId") {
+        return res.status(404).json({
+          message: "User not found with provided ID"
+        });
+      }
+      res.status(500).json({
+        message: err.message || "Some error occurred while retrieving User."
+      });
+    });
+}; // end getAll
