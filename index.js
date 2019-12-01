11b6eeba3bb3c64f716a6b03eae864e5b81e4ab4
PtatG <55119325+PtatG@users.noreply.github.com>
2019-11-30 19:49:01 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,45 +1,42 @@
+// written by: Phillip Tat from GROUP 21(Trevor Bland, Zach Arehart, Rob Lee, and Phillip Tat)
+// date written: 11/23/19
+// course: COP 4331(Rick Leinecker)
+// purpose: Large Project: Focus Pocus(api)
 const express = require("express");
 const bodyParser = require("body-parser");
-
-// create express app
+const mongoose = require("mongoose");
+const path = require("path");
+const config = require("config");
+const fs = require("fs");
 const app = express();
-
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+const db = config.get("remote");
+const port = 3000;
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 
-// Configuring the database
-const dbConfig = require("./config/database.config.js");
-const mongoose = require("mongoose");
-
-mongoose.Promise = global.Promise;
-
 // Connecting to the database
-mongoose
-  .connect(dbConfig.url, {
-    useNewUrlParser: true
-  })
-  .then(() => {
-    console.log("Successfully connected to the database");
-  })
-  .catch(err => {
-    console.log("Could not connect to the database. Exiting now...", err);
-    process.exit();
-  });
+mongoose.connect(db, {
+  useNewUrlParser: true, useUnifiedTopology: true,
+  useCreateIndex: true, useFindAndModify: false
+})
+.then(() => {
+  console.log("Successfully connected to MongoDB");
+})
+.catch(err => {
+  console.log(err);
+  process.exit();
+});
 
 // define a simple route
 app.get("/", (req, res) => {
-  res.json({
-    message: "Welcome to Focus Pocus."
-  });
+  res.json({message: "Welcome to Focus Pocus."});
 });
 
-// Require Notes routes
 require("./app/routes/user.routes.js")(app);
+require("./app/routes/routine.routes.js")(app);
 
 // listen for requests
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
+app.listen(port, () => {
+  console.log("Server is listening on port %d", port);
 });
